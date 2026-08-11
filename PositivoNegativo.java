273c2cc8f02e739223a2660bb35b1632410d87c3
Gabriel Cardoso Possamai <gabriel.cardoso.possamai@gmail.com>
2026-08-11 14:46:49 -0300

    Add files via upload

---
+++ PositivoNegativo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner 
+
+public class PositivoNegativo {
+    public static void PositivoNegativo(String[]args){
+        Scanner ler = new Scanner(System.in);
+        int n = ler.nextInt();
+        if (n > 0){
+        System.out.println("O numero é positivo");
+        } else (n < 0) {
+            System.out.println("é negativo");
+        } else {
+            System.out.println("é igual a zero");
+            }  
+        }
+     
+    }
+}
