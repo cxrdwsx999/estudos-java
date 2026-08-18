f4d4ed5dc73a56f6b04e5dbdc59c80222476558e
Gabriel Cardoso Possamai <gabriel.cardoso.possamai@gmail.com>
2026-08-18 20:15:30 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("Digite o primeiro número:");
+            double num1 = ler.nextDouble();
+        
+        System.out.println("Digite o segundo número:");
+            double num2 = ler.nextDouble();
+        
+        System.out.println("Digite a operação (+, -, *, /):");
+            String operacao = ler.next();
+        if (operacao.equals("+")) {
+            double resultado = num1 + num2;
+        System.out.println("Resultado: " + resultado);
+        }else if (operacao.equals("-")) {
+            double resultado = num1 - num2;
+        System.out.println("Resultado: " + resultado);
+        }else if (operacao.equals("*")){
+            double resultado = num1 * num2;
+        System.out.println("Resultado: " + resultado);
+        }else if (operacao.equals("/")) {
+            double resultado = num1 / num2;
+        System.out.println("Resultado: " + resultado);    
+        }else{
+        System.out.println("OPERAÇÃO INVÁLIDA");
+        }
+    
+        
+    }
+}
